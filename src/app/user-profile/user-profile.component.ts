f4415e324440143c21b3b55393b62cb7365283e3
sashu-sashu <iamsashaalexandra@google.com>
2022-09-18 16:46:46 +0200

    added missing part in profile component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,7 +1,7 @@
 /*eslint-disable @angular-eslint/no-empty-lifecycle-method*/
 import { Component, OnInit, Input } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 
@@ -10,7 +10,16 @@ type User = {
   Password: string;
   Email: string;
   Birth: string;
+  FavouriteMovies: string[]
 };
+
+const initalUser: User = {
+    Username: '',
+    Password: '',
+    Email: '',
+    Birth: '',
+    FavouriteMovies: []
+  }
 
 @Component({
   selector: 'app-user-profile',
@@ -18,26 +27,28 @@ type User = {
   styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
+  user: User = initalUser;
+  movies: any[] = [];
+  filteredMovies: any[] = [];
   /**
    * Shows UserProfile page thatnks to its route reference 'profile'.
    * The data it's partial because the user might change their data just partially.
    * @param data 
    */
-  @Input() userData: Partial<User> = {
-    Username: '',
-    Password: '',
-    Email: '',
-    Birth: '',
-  };
+  @Input() userData: Partial<User> = initalUser;
 
   constructor(
     public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
     public dialogRef: MatDialogRef<UserProfileComponent>,
     public snackBar: MatSnackBar,
     public router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getUser();
+    this.getFavoriteMovies();
+  }
 
   //allowing user to edit details
   editUser(): void {
@@ -48,5 +59,83 @@ export class UserProfileComponent implements OnInit {
       // Log out user if they update Username or Password to avoid errors
       this.router.navigate(['welcome']);
     });
+  }
+
+  /**
+   * @function getUser
+   * opens dialog to get user information 
+   */
+  getUser(): void {
+    this.fetchApiData.getUser().subscribe((resp: any) => {
+      this.user = resp;
+      console.log(this.user);
+      return this.user;
+    })
+  }
+
+  /**
+   * @function openEditProfileDialog
+   * opens dialog to allow user to edit information
+   */
+  openEditProfileDialog(): void {
+    this.dialog.open(UserProfileComponent, {
+      width: '300px'
+    })
+  }
+
+  /**
+   * @function deleteProfile
+   * opens dialog to delete user and clear the stored user information
+   */
+  deleteProfile(): void {
+    if (confirm('Are you sure u want to delete your account?')) {
+      this.router.navigate(['welcome']).then(() => {
+        this.snackBar.open('successfully deleted the account', 'OK', {
+          duration: 2000
+        });
+      })
+      this.fetchApiData.deleteUser().subscribe((result) => {
+        console.log(result);
+        localStorage.clear();
+      });
+    }
+  }
+
+  /**
+   * @function getFavoriteMovies
+   * opens dailog to add movie to user favorite movies list
+   */
+  getFavoriteMovies(): void {
+    this.fetchApiData
+      .getAllMovies().subscribe((response: any) => {
+        this.movies = response;
+        this.filteredMovies = this.filterMovies(this.movies, this.user.FavouriteMovies);
+        return this.filteredMovies;
+      });
+  }
+
+  filterMovies(movies: any, FavouriteMovies: any): any {
+    let remainingMovies = [];
+    for (let movie in movies) {
+      if (FavouriteMovies.includes(movies[movie]._id)) {
+        remainingMovies.push(movies[movie]);
+      }
+    }
+    return remainingMovies;
+  }
+
+  /**
+   * @function removeFavoriteMovie
+   * 
+   * @param movie_id 
+   * @returns removes selected movie from user favorite movies list 
+   */
+  removeFavoriteMovie(movie_id: string): void {
+    this.fetchApiData
+      .removeFavoriteMovie(movie_id)
+      .subscribe((response) => {
+        console.log(response);
+        window.location.reload();
+      });
   }
 }
